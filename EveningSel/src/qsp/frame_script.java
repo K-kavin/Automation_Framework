2adcb51489f3d1fe3e43853d5caa41b70d5a0654
QSPR <QSPR@AUTOMATION-LAB>
2023-09-01 17:11:22 +0530

    Build B03

--- EveningSel/src/qsp/frame_script.java
+++ EveningSel/src/qsp/frame_script.java
@@ -13,16 +13,16 @@ public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new FirefoxDriver();
 	driver.get("https://www.naukri.com/nlogin/login");
 	String p_id = driver.getWindowHandle();
-	Thread.sleep(1000);
+	//Thread.sleep(1000);
 	driver.findElement(By.xpath("//a[@href='https://resume.naukri.com/?fftid=100001']")).click();
-	Thread.sleep(1000);
+	//Thread.sleep(1000);
 	Set<String> allwh = driver.getWindowHandles();
 	int size = allwh.size();
 	System.out.println(size);
 	ArrayList<String> l1=new ArrayList<String>(allwh);
 	String child_id = l1.get(1);
 	driver.switchTo().window(child_id);
-	Thread.sleep(2000);
+	//Thread.sleep(2000);
 	
 	driver.findElement(By.xpath("//a[@title='CONTACT US']")).click();
 	
